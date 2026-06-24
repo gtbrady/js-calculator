87b89c2eb1aa46db61768320f346b6e53c29e6a9
gtbrady <97182053+gtbrady@users.noreply.github.com>
2026-06-24 09:20:26 -0500

    Add basic math functions and global variables

--- calculator.js
+++ calculator.js
@@ -1,1 +1,25 @@
-console.log("Hey");
+let numberOne;
+let numberTwo;
+let operator;
+
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if(num2 === 0) {
+        return "Cannot divide by 0";
+    }
+    else {
+        return num1 / num2;
+    }
+}
