4421c75555e143a9ad0beb0d43585fdf3f54edf3
gtbrady <97182053+gtbrady@users.noreply.github.com>
2026-07-07 09:26:33 -0500

    Add references to buttons
    ID selector used for buttons with shared CSS styling

--- calculator.js
+++ calculator.js
@@ -1,6 +1,37 @@
+//global
 let globalNumberOne;
 let globalNumberTwo;
 let globalOperator;
+
+//display
+const outputDisplay = document.querySelector(".output");
+
+//number keys
+const numButton1 = document.getElementById("btn-1");
+const numButton2 = document.getElementById("btn-2");
+const numButton3 = document.getElementById("btn-3");
+const numButton4 = document.getElementById("btn-4");
+const numButton5 = document.getElementById("btn-5");
+const numButton6 = document.getElementById("btn-6");
+const numButton7 = document.getElementById("btn-7");
+const numButton8 = document.getElementById("btn-8");
+const numButton9 = document.getElementById("btn-9");
+const numButton0 = document.getElementById("btn-0");
+
+//operators
+const divideButton = document.getElementById("btn-divide");
+const multiplyButton = document.getElementById("btn-multiply");
+const subtractButton = document.getElementById("btn-subtract");
+const addButton = document.getElementById("btn-add");
+
+//other buttons
+const negationButton = document.getElementById("btn-negation");
+const decimalButton = document.getElementById("btn-decimal");
+const backspaceButton = document.getElementById("btn-backspace");
+
+//class selected buttons
+const clearButton = document.querySelector(".clear-button");
+const equalButton = document.querySelector(".equal-button");
 
 
 function add(num1, num2) {
